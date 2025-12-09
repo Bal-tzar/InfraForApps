84a7f309e81ff5d06168d23d6ca099948a339b04
Bal-tzar <ottodemare1@gmail.com>
2025-12-09 16:43:05 +0100

    actual final commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cd ../terraform
 
 terraform init
 terraform apply -var="postgres_admin_password=${POSTGRES_PASSWORD}" -auto-approve
-#sleep 10m
+
 
 az aks get-credentials --resource-group paytrackr-rg --name paytrackr-aks --overwrite-existing
 
